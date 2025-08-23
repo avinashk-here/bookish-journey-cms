22480a1383fecc8d3a6f158d52f52af2eb2084df
Avinash Khodke <imavinashhere@gmail.com>
2025-08-23 17:18:45 +0530

    Update Cell.js
    server changed to deplyed bkend

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -27,7 +27,7 @@ export default function Cell({
     setLoading(true);
     try {
       const res = await fetch(
-        `${process.env.REACT_APP_API_URL || "http://localhost:5000"}/api/generate`,
+        "https://cms-api-6ihr.onrender.com/api/generate",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
